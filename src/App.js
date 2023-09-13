a3afefe1e92aa63d4109b2e0392b69cfd6651cf2
Netta <n2ojne00@students.oamk.fi>
2023-09-13 18:11:46 +0300

    kauppapohja

--- src/App.js
+++ src/App.js
@@ -1,24 +1,46 @@
-import logo from './logo.svg';
+
 import './App.css';
+import {FaShoppingCart} from 'react-icons/fa'
+import {useState} from 'react';
+
+
 
 function App() {
+  const [cart,setCart] = useState([])
+
+  const [products, setProducts] = useState([
+    {name: "Jacket", price: 100},
+    {name: "Pänts", price: 140}, 
+    {name: "Hattu", price: 90} 
+  ])
+
+  const buy = (e, index) => {
+    e.preventDefault()
+    const product = products[index]
+    setCart([...cart,product])
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <div id="container">
+    <h2>TOSI ÄIÄN VAATEKAUPPA</h2>
+    <p><FaShoppingCart/>{cart.length} tuote</p>
+   
+   {
+    products.map((product,index) =>(
+      <div key={index}>
+        <form onSubmit={(e)=> buy(e,index)}>        
+        <h3>{product.name}</h3>
+        <p>{product.price} €</p>
+        <button>BUY</button>
+        </form>
+      </div>
+    ))
+   }
+   
+   </div>
+
+
+
   );
 }
 
